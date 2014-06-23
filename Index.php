4ba10d5beeca877fd44510c8ac1f2eb5eeaa35d3
seksenov <seksenovk@gmail.com>
2014-06-22 19:27:57 -0700

    testing PHP 2

---
+++ Index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>First Web Page</title>
+    </head>
+    
+    <body>
+        <h1>About Yolo Problems</h1>
+        
+        <?php
+        echo "Hello World!";
+        ?>
+
+        <p>Yolo Problems occur when one engages in yolo too hard and ensuing problems follow</p>
+        <h2>Some Yolo Problems Examples</h2>
+        <ul>
+            <li>Drink too much and miss work</li>
+            <li>Work Hard Play Hard</li>
+            <li>Cashin Out</li>
+            <li>YOLO Hard</li>
+            <li>Yolo in my 'Stang</li>
+        </ul>
+        
+        <div id="imageMustang">
+            <img src="myMustang.JPG" width="40%" alt="2014 Ford Mustang GT Picture"/><br/>
+            <caption>2014 Ford Mustang GT</caption>
+        </div>
+        
+        <footer>Copyright 2014 <a href="http://www.google.com">YoloProblems.com</a>
+        
+        </footer>
+        
+    </body>
+</html>
