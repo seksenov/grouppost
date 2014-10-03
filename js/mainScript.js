3131a5a3264ed227841106e5af36691cc2a4f722
seksenov <seksenovk@gmail.com>
2014-10-02 23:12:16 -0700

    getting rid of FB login

--- js/mainScript.js
+++ js/mainScript.js
@@ -3,7 +3,7 @@ var app = angular.module('postItApp', []);
 
 var idNum;
 
-var userID;
+var userID = '10152621751097597';
 
 var notesArray = [];
 
@@ -19,63 +19,6 @@ var client = new WindowsAzure.MobileServiceClient(
 
 var userTable=null;
 userTable=client.getTable("userTable");
-
-
-
-//do the FB init stuff
-//function checkLoginState() {
-//    FB.getLoginStatus(function(response) {
-//      statusChangeCallback(response);
-//    });
-//  }
-
-window.fbAsyncInit = function() {
-
-  console.log("Yo! ------------------ initializing FB");
-
-  FB.init({
-    appId      : '821945741172950',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.1' // use version 2.1
-
-
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  //FB.getLoginStatus(function(response) {
-  //  statusChangeCallback(response);
-  //});
-
-  console.log("Yo! ------------------ about to get the uid");
-
-  FBuid();
-
-};
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-//Get the user FB uid of the person logged in
-
 
 //var item = { text: "1: This is Static" };
 //client.getTable("Item").insert(item);
@@ -106,36 +49,8 @@ function selectCanvas(canvasID)
 
 }
 
-function FBLogout() {
-  FB.logout(function(response) {
-        console.log("Person is now logged out");
-        window.location.href = "Index.html";
-        console.log("They should also be redirected");
-  });
-}
-
-function FBuid() {
-
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      userID = response.authResponse.userID;
-      console.log('Logged in.');
-      console.log('The user id is: ' + userID);
-      //Get all the post it's from the DB and display them on the page
-      getPostIts();
-    }
-    else {
-      //FB.login();
-      console.log('Not logged in');
-      window.location.href = "Index.html";
-    }
-  });
-}
-/*
-$(document).on('click', 'div', function () {
-    alert(this.id);
-});
-*/
+getPostIts();
+  
 
 function selectDiv(divID, buttonID, isPlus, dcID)
 {
