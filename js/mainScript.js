b60938a2d4e823e8d01b58b8fa81d62c70cec157
seksenov <seksenovk@gmail.com>
2015-02-07 00:02:19 -0800

    fixing bug where element is contained

--- js/mainScript.js
+++ js/mainScript.js
@@ -131,8 +131,13 @@ function FBuid() {
           //var dcID = "dc" + getParent(changedChild).val().divNum;
 
           console.log("This is the parent: ---+++---+++___+++___++___++___+++___++_+++" + getParent(changedChild));
+          var idArray = getParent(changedChild)).split('v');
+          var dcID = "dc" + idArray[1];
+          console.log("This is what the dcID is: " + dcID);
 
-          updatePosts(getParentName(changedChild), changedChild.name(), changedChild.val());
+
+
+          updatePosts(getParentName(changedChild), changedChild.name(), changedChild.val(), dcID);
         });
 
         var image = null;
